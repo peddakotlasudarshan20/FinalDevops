61c7cc0fa66110ea05014de23390491ddf33e43c
peddakotlasudarshan20 <peddakotlasudarshan@gmail.com>
2026-06-05 19:59:36 +0530

    python

--- sample-flask-with-kubernetes/app/main.py
+++ sample-flask-with-kubernetes/app/main.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 env = os.environ.get('VERSION', '--')
 
-
 @app.route('/')
 def root():
     return render_template(
         'index.html',
         message=f'You accessed the {env} version of the app.',
     )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
